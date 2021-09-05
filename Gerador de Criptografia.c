79b3f4f33cbf9e6953bfe1a9bcb2b227cd3cb887
Dante Dantas <70487982+danttis@users.noreply.github.com>
2021-09-05 20:04:13 -0300

    dictionary randomizatio
    dictionary randomization, enhanced encryption security

--- Gerador de Criptografia.c
+++ Gerador de Criptografia.c
@@ -13,36 +13,70 @@ system("pause");
 int main(){   
 	char nome[50];
   char crypter[50];
-  int dicionario[50] = {9,3,4,7,9,5,4,1,8,2,0,1,3,4,4,7,2,9,5,9,0,1,3,6,8,6,1,8,2,0,3,4,5,9,3,5,1,2,8,9,1,1,7,6,8,3,5,2,0,9}; //O dicionario pode ser mudado na preferência do usuário, visando criar criptografias mais sofisticadas;  
+  int dic[50];  
   char x;
   setlocale(LC_ALL, ""); //Uso das acentuações no terminal
   printf("Deseja Criptografar(c) ou Descriptografar(d)?: ");
-  scanf("%c",&x);
+  while(x != 'c' && x != 'd'){
+       scanf("%c",&x);
+       if( x!= 'c' && x!= 'd')
+       printf("Caractere não aceito\n");
+  }
+ 
 
   if(x == 'c'){
-    printf("Digite o que deseja criptogafar: ");
+    printf("Ja tem um dicionario?\n Sim(s), Nao(n)?\n");
+    scanf("%c",&x);
+    while(x != 's' && x != 'n'){
+        scanf("%c",&x);
+        if( x!= 's' && x!= 'n')
+          printf("Caractere não aceito\n");
+    }
+    if(x == 's'){
+      FILE *dictionary;
+      dictionary = fopen("dictionary.txt", "a");
+      for(int i = 0 ; i < 50; i++){
+        fscanf(dictionary, "%d", &dic[i]);
+      }
+    }
+    if(x == 'n'){
+      FILE *dictionary;
+      dictionary = fopen("dictionary.txt", "a");
+      for(int i = 0; i < 50; i++){
+        dic[i] = rand() % 9;
+        fprintf(dictionary, "%d\n",dic[i]);
+      }
+      fclose(dictionary);
+    }
+    printf("\nDigite o que deseja criptogafar: \n");
     fgets(nome, 49, stdin);//Limpando (a)/(enter) do buffer do teclado 
     fgets(nome, 49, stdin); 
     for(int i = 0; i < strlen(nome) ; i++){
-      crypter[i] = nome[i] + dicionario[i+2];//Strings podem ser incrementadas ou decrementadas mudando assim para outro valor na Tabela ASCII
+      crypter[i] = nome[i] + dic[i];//Strings podem ser incrementadas ou decrementadas mudando assim para outro valor na Tabela ASCII
     }
     puts(crypter);
 
   }
  else if (x == 'd'){
+   FILE *dictionary;
+   dictionary = fopen("dictionary.txt", "r");
+   if(dictionary == NULL){
+   printf("Você não tem um dicionário, impossivél descriptografar");}
+   else{
+   for(int i = 0 ; i < 50; i++){
+        fscanf(dictionary, "%d", &dic[i]);
+      }
+    }
    printf("Digite o que deseja descryptografar: ");
    fgets(crypter, 49, stdin);
    fgets(crypter, 49, stdin);
    for(int i = 0; i < strlen(crypter) ; i++){
-      nome[i] = crypter[i] - dicionario[i+2];
+      nome[i] = crypter[i] - dic[i];
     }
     puts(nome);
  }
- else{
-      printf("Caractere não aceito\n");
- }
+ 
   
   pause();
 	return (0);
 }
-
